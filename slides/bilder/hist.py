2c46ba35e95cec228f47ee2fdd12d1d4da4bc6f2
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2014-11-10 19:59:47 +0100

    update

--- slides/bilder/hist.py
+++ slides/bilder/hist.py
@@ -1,8 +1,25 @@
 import cv2
 import numpy as np
+import huffman
 from matplotlib import pyplot as plt
 
-img = cv2.imread("uniformnoise.png", 0)
-plt.hist(img.ravel(), 256, [0,256], fc='k', ec='k')
-plt.xlim([0, 256])
-plt.savefig("uniformnoise_hist.eps", format="eps", bbox_inches='tight')
+sum = 0
+bits = 0
+
+img = cv2.imread("moon.jpg", 0)
+values = [0 for i in range(0, 256)]
+for value in img.ravel():
+    values[value] += 1
+    sum += 1
+
+for i in range(0, len(values)):
+    p = float(values[i])/sum
+    c = huffman.h[i]
+    bits += values[i]*len(c)
+    print str(i) + "," + str(values[i]) + "," + str(p) + "," + c
+
+print float(bits)/sum
+
+#plt.hist(img.ravel(), 256, [0,256], fc='k', ec='k')
+#plt.xlim([0, 256])
+#plt.savefig("uniformnoise_hist.eps", format="eps", bbox_inches='tight')
